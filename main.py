d08cde63310ee87fb70b0797f250b46c9ce5963c
danxham <notrackersonme@duck.com>
2024-09-13 09:59:11 -0400

    cleaned up code

--- main.py
+++ main.py
@@ -1,35 +1,29 @@
 def main():
     book_path = "books/frankenstein.txt"
-    text = get_book_text(book_path)
+    text = get_book_path(book_path)
     word_count = count_words(text)
     character_count = count_characters(text)
-    print(f"Word count: {word_count}")
+    print(text)
+    print(word_count)
     print(character_count)
 
 
-def get_book_text(path):
+def get_book_path(path):
     with open(path) as f:
         return f.read()
-    
+
 def count_words(text):
-    running_count = 0
-    for word in text.split():
-        running_count += 1
-    return  running_count
+    words = len(text.split())
+    return words
 
 def count_characters(text):
-    character_dictionary = {}
-    lower = text.lower()
-    for i in lower:
-        if i in character_dictionary:
-            character_dictionary[i] += 1
+    lowered_string = text.lower()
+    count = {}
+    for i in lowered_string:
+        if i in count:
+            count[i] += 1
         else:
-            character_dictionary[i] =1
-    return character_dictionary
-
-
-
-
-
-
+            count[i] = 1
+    return count
+    
 main()
